6947df4844291cbeb56b81e4c30dd3f9c35d18e3
Rodolfo Pereira <phinpho@gmail.com>
2017-05-03 10:51:44 -0300

    Adds tileset component to handle a tile draw in canvas

---
+++ src/app/projects/map/tileset.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-mondo-tileset',
+    templateUrl: './tileset.component.html',
+    styleUrls: ['./tileset.component.scss']
+})
+export class TilesetComponent {
+
+}
